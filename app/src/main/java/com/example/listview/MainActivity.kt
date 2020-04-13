10c993231d73de7cae58ca16c20be9bbcf065615
eunjung04 <peunj04@gmail.com>
2020-04-13 20:50:03 +0900

    실 데이터 리스트에 반영

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -16,9 +16,12 @@ class MainActivity : AppCompatActivity() {
 
         roomList.add(Room(8000,"서울시 은평구", 4,"연립주택입니다."))
         roomList.add(Room(8000,"서울시 영등포구", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"서울시 노량진구", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"경기도 수원시", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"경기도 남양주시", 4,"연립주택입니다."))
+        roomList.add(Room(15000,"서울시 노량진구", 4,"연립주택입니다."))
+        roomList.add(Room(14000,"경기도 수원시", 4,"연립주택입니다."))
+        roomList.add(Room(17000,"경기도 남양주시", 4,"연립주택입니다."))
+        roomList.add(Room(8000,"경기도 화성시", 4,"연립주택입니다."))
+        roomList.add(Room(8000,"경기도 안양시", 4,"연립주택입니다."))
+        roomList.add(Room(8000,"경기도 구리시", 4,"연립주택입니다."))
 
 
 
--- app/src/main/java/com/example/listview/RoomAdapter.kt
+++ app/src/main/java/com/example/listview/RoomAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
 class RoomAdapter(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapter<Room>(context, resId, list)  {
 
@@ -20,6 +21,44 @@ class RoomAdapter(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapt
         }
 
         val row = tempRow!!
+
+      //XML에 있는 뷰들을 변수로 담기(findViewById)
+
+        val price=row.findViewById<TextView>(R.id.price)
+        val addressAndFloor=row.findViewById<TextView>(R.id.addressAndFloor)
+        val description=row.findViewById<TextView>(R.id.description)
+
+        //근거 데이터 추출.=> 각 위치에 맞는 데이터를 mList에서 가져오기.
+
+        val data=mList.get(position)
+
+        //가격이 1만 이상? ? "?억 ?"식, 그 이하 "?"
+
+        if(data.price>=10000) {
+
+            price.text = "${data.price / 10000}억 ${String.format("%,d"data.price % 10000)}"
+        }
+        else {
+            price.text= String.format("%,d",data.price)
+        }
+
+        //주소와 층수=>"주소, 층수(상황마다 다른값)
+
+        var floorStr=""
+        id(data.floor==0){
+            floorStr="반지하"
+        }
+        else  (data.floor > 0){
+            floorStr="${data.floor}층"
+        }
+        else if{
+            floorStr="지하${data.address}층"
+        }
+
+        addressAndFloor.text="${data.address}, ${floorStr}"
+
+        description.text=data.desc
+
 
         return row
 
