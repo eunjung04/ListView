646bf873ecb17077a172730c84e7db93e20b9c23
eunjung04 <peunj04@gmail.com>
2020-04-13 20:53:42 +0900

    메인엑티비티 가격 수정

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -15,13 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         roomList.add(Room(8000,"서울시 은평구", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"서울시 영등포구", 4,"연립주택입니다."))
+        roomList.add(Room(18300,"서울시 영등포구", 4,"연립주택입니다."))
         roomList.add(Room(15000,"서울시 노량진구", 4,"연립주택입니다."))
         roomList.add(Room(14000,"경기도 수원시", 4,"연립주택입니다."))
         roomList.add(Room(17000,"경기도 남양주시", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"경기도 화성시", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"경기도 안양시", 4,"연립주택입니다."))
-        roomList.add(Room(8000,"경기도 구리시", 4,"연립주택입니다."))
+        roomList.add(Room(13400,"경기도 화성시", 4,"연립주택입니다."))
+        roomList.add(Room(12000,"경기도 안양시", 4,"연립주택입니다."))
+        roomList.add(Room(12500,"경기도 구리시", 4,"연립주택입니다."))
 
 
 
