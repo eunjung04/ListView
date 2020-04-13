e9ae860564e046b2d31966b7f70b707b09bd64ff
eunjung04 <peunj04@gmail.com>
2020-04-13 20:23:05 +0900

    리스트뷰 뼈대 작업

--- app/src/main/java/com/example/listview/RoomAdapter.kt
+++ app/src/main/java/com/example/listview/RoomAdapter.kt
@@ -1,33 +1,28 @@
 package com.example.listview
 
+
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.example.listview
-import com.example.listview
 
-class RoomAdapter (context: Context, resId: Int, list: ArrayList<Room>): ArrayAdapter<Room>(context, resId, list) {
+class RoomAdapter(context:Context, resId:Int, list:ArrayList<Room>) : ArrayAdapter<Room>(context, resId, list)  {
 
-val mContext : context
-val mList= list
-val inf=LayoutInflater.from(mContext)
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
 
-override fun getView(position: Int, convertView : View?, parent: ViewGroup): View {
+        val row = tempRow!!
 
-val tempRow=convertView
-if(tempRow==null){
-    tempRow = inf.inflate(R.layout.room_list_item, null)
+        return row
 
-}
-
-val row=tempRow!!
-//row 변수를 활용해서
-
-return row
-
-}
+    }
 
 }
